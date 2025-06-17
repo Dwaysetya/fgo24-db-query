e2535ef89af74740aabed59ca702230182aa3ae4
dwisetbu@gmail.com <dwisetbu@gmail.com>
2025-06-17 17:08:14 +0700

    feat: answer question number 8

--- challenge3.sql
+++ challenge3.sql
@@ -23,9 +23,7 @@ ORDER BY
 
 
 -- 7. Mendapatkan data actors yang memiliki role lebih dari 5
-SELECT *
-FROM (
-    SELECT
+SELECT 
     a.id,
     a.first_name,
     a.last_name,
@@ -36,12 +34,50 @@ JOIN
     roles r ON r.actor_id = a.id
 GROUP BY
     a.id, a.first_name, a.last_name
-    ) AS actor_roles
-WHERE 
-    total_roles > 5
+HAVING
+    COUNT(DISTINCT r.role)>5
 ORDER BY 
     total_roles ASC;
 
+
+
+-- 8. Mendapatkan directors paling produktif sepanjang masa
+-- 10 terbaik
+SELECT 
+    d.id,
+    d.first_name,
+    d.last_name,
+    COUNT(d.id) AS total_movies
+FROM 
+    directors d
+JOIN 
+    movies_directors md ON md.director_id = d.id
+GROUP BY 
+    d.id, d.first_name, d.last_name
+ORDER BY 
+    total_movies DESC
+LIMIT 10;
+
+-- range tahun 2000 - 2004 directors tersibuk
+SELECT 
+    d.id,
+    d.first_name,
+    d.last_name,
+    COUNT(md.movie_id) AS total_movies,
+    MIN(m.year) AS first_year,
+    MAX(m.year) AS last_year
+FROM 
+    directors d
+JOIN 
+    movies_directors md ON d.id = md.director_id
+JOIN 
+    movies m ON m.id = md.movie_id
+WHERE
+    m.year BETWEEN 2000 AND 2004
+GROUP BY 
+    d.id, d.first_name, d.last_name
+ORDER BY 
+    total_movies DESC
 
 
 
