feae89be90e5ddd247e035471b66a23d8039508c
dwisetbu@gmail.com <dwisetbu@gmail.com>
2025-06-17 16:43:03 +0700

    feat: answer question number 7

--- challenge3.sql
+++ challenge3.sql
@@ -2,19 +2,48 @@
 -- 7. Mendapatkan data actors yang memiliki role lebih dari 5
 -- 8. Mendapatkan directors paling produktif sepanjang masa
 -- 9. Mendapatkan tahun tersibuk sepanjang masa
+-- 10. Mendapatkan movies dengan genres yang dibuatkan menjadi 1 column
+-- (value dipisahkan dengan koma) dengan menggunakan string_agg
 
+
+-- 6. Mendapatkan data director, berapa genre yang di-derect
 SELECT 
     d.id,
     d.first_name,
     d.last_name,
     COUNT(DISTINCT dg.genre) AS total_genres
-FROM directors d
-JOIN directors_genres dg on dg.director_id = d.id
-GROUP BY d.id, d.first_name,d.last_name
-ORDER BY total_genres
+FROM 
+    directors d
+JOIN 
+    directors_genres dg ON dg.director_id = d.id
+GROUP BY 
+    d.id, d.first_name, d.last_name
+ORDER BY 
+    total_genres DESC;
 
 
-SELECT * FROM directors_genres LIMIT 10;
+-- 7. Mendapatkan data actors yang memiliki role lebih dari 5
+SELECT *
+FROM (
+    SELECT
+    a.id,
+    a.first_name,
+    a.last_name,
+    COUNT(DISTINCT r.role) AS total_roles
+FROM 
+    actors a 
+JOIN 
+    roles r ON r.actor_id = a.id
+GROUP BY
+    a.id, a.first_name, a.last_name
+    ) AS actor_roles
+WHERE 
+    total_roles > 5
+ORDER BY 
+    total_roles ASC;
+
+
+
 
 
 
